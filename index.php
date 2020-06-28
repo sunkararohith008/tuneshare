649e500078968538260262a38aac81284f967248
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-06-28 14:47:00 -0400

    update index.php
    changed text

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
     <p class="lead">Need a new WFH playlist? We've got you covered. Join our community and connect with fellow music-lovers. Share your top bops and browse what others are listening too. </p>
     <!-- I had to Google what bop meant. I am very uncool -->
     <p class="lead">
-      <a href="add.php" class="btn btn-lg btn-secondary">Share Your Playlist</a>
-      <a href="#" class="btn btn-lg btn-secondary orange">Browse Playlists </a>
+      <a href="add.php" class="btn btn-lg btn-secondary">Share Your Tune</a>
+      <a href="#" class="btn btn-lg btn-secondary orange">Browse Tune </a>
     </p>
   </main>
   <footer class="mastfoot mt-auto">
