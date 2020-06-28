a4645f1b003c07b35290c7e76122958e12f49964
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-06-28 14:44:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <p class="lead">Need a new WFH playlist? We've got you covered. Join our community and connect with fellow music-lovers. Share your top bops and browse what others are listening too. </p>
     <!-- I had to Google what bop meant. I am very uncool -->
     <p class="lead">
-      <a href="add.php" class="btn btn-lg btn-secondary">Share Your Playlist</a>
+      <a href="add.php" class="btn btn-lg btn-secondary">Share Your Top Tune</a>
       <a href="#" class="btn btn-lg btn-secondary orange">Browse Playlists </a>
     </p>
   </main>
